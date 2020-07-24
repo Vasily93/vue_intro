2b1031281c5879dbf8646b4e3570fae34caa4835
Vasily93 <markovavasily@gmail.com>
2020-07-23 18:17:56 -0700

    finished swapi part 2

--- 17_starWars_search/app.js
+++ 17_starWars_search/app.js
@@ -1,3 +1,22 @@
+const swapi = (function(axios) {
+    const baseApi = 'https://swapi.dev/api/';
+
+    const searchPeople = (person) => {
+        const url = `${baseUrl}people/?search=${person}&format=json`;
+        return axios.get(url)
+                    .then(responce => console.log(responce));
+    };
+    const getPersonDeatail = (url) => {
+        return axios.get(url)
+                    .then(responce => console.log(responce));
+    };
+
+    return {
+        searchPeople,
+        getPersonDeatail
+    }
+})(axios='axios');
+
 // compomnents are here
 const SearchBar = {
     template: '#search-bar',
